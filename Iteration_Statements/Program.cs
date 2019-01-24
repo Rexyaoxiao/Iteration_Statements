bcc90d9a651c88293909246e4b9cf73346fec5c3
Yao Xiao <Yao Xiao@DESKTOP-AT0LLHN>
2019-01-24 16:15:29 -0500

    Yao

--- Iteration_Statements/Program.cs
+++ Iteration_Statements/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+    Author: Yao Xiao
+    Date: 1/24/2019
+    Comments: This C# Console application code demonstrates the use of
+              iterative statements after getting input from users. 
+*/
+using System;
 
 namespace Iteration_Statements
 {
@@ -6,7 +12,46 @@ namespace Iteration_Statements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an Integer value between 1 and 100 to execute an iterative statement:");
+            try
+            {
+                string input = Console.ReadLine();
+                int value_of_input = int.Parse(input);
+
+                if ((value_of_input > 0) && (value_of_input <= 100))
+                {
+                    Console.WriteLine("Executing a For Loop!");
+                    Console.WriteLine("The For Loop will iterate " + value_of_input.ToString() + " times.");
+
+                    for (int i = 0; i < value_of_input; i++)
+                    {
+                        Console.WriteLine("The value of the variable i in this iteration is: " + i.ToString());
+                    }
+                    Console.WriteLine("Press any key to exit the program ...");
+
+                    Console.ReadKey(true);
+                }
+                else if ((value_of_input > 0) && (value_of_input <= 100))
+                {
+                    for (int i = 0; i > value_of_input; i++)
+                    {
+                        Console.WriteLine("The value of the iterative variable is: " + value_of_input.ToString());
+                        value_of_input--;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an integer between 1 and 100 and try again ...");
+                    Console.WriteLine("Press any key to exit the program and try again ...");
+                    Console.ReadKey(true);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer value and try running the program again ...");
+                Console.WriteLine("Press any key to exit the program ...");
+                Console.ReadKey(true);
+            }
         }
     }
 }
